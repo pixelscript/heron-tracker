3407c12fbf7345f059a355ae93ef6394764497ac
Chris Corbett <ccorbett@amplience.com>
2024-05-07 17:59:41 +0100

    feat: updating model, making poller more fault tollerant

--- polling_image_test.py
+++ polling_image_test.py
@@ -29,8 +29,12 @@ def img_to_base64(img):
 
 # Function to make an API call
 def call_api(image):
-    image_data = img_to_base64(image)
-    requests.post(endpoint, json={'image': image_data, 'message': 'Heron detected multiple times'})
+    try:
+        image_data = img_to_base64(image)
+        requests.post(endpoint, json={'image': image_data, 'message': 'Heron detected multiple times'})
+    except Exception as e:
+        print(f"Error: {e}")
+        return None   
 
 # Function to fetch and return image
 def fetch_image():
@@ -38,25 +42,42 @@ def fetch_image():
     try:
         image = requests.get(imageEndpoint)
         image = cv2.imdecode(np.frombuffer(image.content, np.uint8), cv2.IMREAD_COLOR)
-        return image
+        width = 640
+        height = 384
+        image = cv2.resize(image, (width, height))
+        return [image, width, height]
     except Exception as e:
         print(f"Error: {e}")
-        return None
-
-while True:
-    image = fetch_image()
-    if image is not None:
-        # Predictions from the model
-        results = model(image, conf=0.8, device='mps', verbose=True)
-        
+        return [None, 0, 0]
+    
+def test_image(image, width, height):
+    global count
+    try:
+        results = model(image, conf=0.8, device='mps', verbose=True, imgsz=(height, width))
         # Process each result
         for r in results:
             # Check if 'heron' is detected
             if detection_class == r.names[0] and r.boxes.conf.numel():
                 count += 1
-
+                return r
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+# Calculate time main loop took
+while True:
+    # Calculate time loop took
+    time_start = cv2.getTickCount()
+    [image, width, height] = fetch_image()
+    if image is not None:
+        # Predictions from the model
+        r = test_image(image, width, height)
         # If heron is detected multiple times, make an API call
+        time_end = cv2.getTickCount()
+        time_loop = (time_end - time_start) / cv2.getTickFrequency()
+        print(f"Loop took {time_loop} seconds")
         if count >= threshold:
-            frame = r.plot()
-            call_api(frame)
+            if (r is not None):
+                frame = r.plot()
+                call_api(frame)
             count = 0
